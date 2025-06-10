1538b93bab5bd5a853fd3d0367d39bc8f695e90b
Kevin Hetzendorfer <kevin.hetzendorfer@accenture.com>
2025-06-10 12:24:08 +0200

    Update react-router.config.ts

--- shadcn-showcase/react-router.config.ts
+++ shadcn-showcase/react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: "/component-library-comparison/heroui-showcase/"
+  basename: "/component-library-comparison/shadcn-showcase/"
 } satisfies Config;
